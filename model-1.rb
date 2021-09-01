fdc5f2c3b1f83c448cb356d31026b07a2c63784c
Parker Lockhart <parkerlockhart42@gmail.com>
2021-09-01 08:37:56 -0500

    Add array variables

--- model-1.rb
+++ model-1.rb
@@ -9,3 +9,7 @@ reward = 704.209
 xaglorkians_allowed = true
 hunting_license_required = false
 primitive_camp_available = true
+
+required_gear = ["med pack", "breathing apparatus", "traps", "lantern", "matter manipulator", "phasor", "cloaking device"]
+checkpoints = ["1", "2", "3", "4"]
+teams = ["red", "blue", "green", "yellow"]
